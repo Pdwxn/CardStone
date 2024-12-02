af6e362a0c5d7e582ebbeae4a5810a7c3a845ee4
Pdwxn <pdwxn@protonmail.com>
2024-12-02 17:25:58 -0500

    Creating options

---
+++ src/components/options/cardSet.ts
@@ -0,0 +1,18 @@
+const cardSets = [
+  "Classic",
+  "Goblins vs Gnomes",
+  "The Witchwood",
+  "Rastakhan's Rumble",
+  "Rise of Shadows",
+  "Saviors of Uldum",
+  "Scholomance Academy",
+  "United in Stormwind",
+  "Ashes of Outland",
+  "Forged in the Barrens",
+  "Descent of Dragons",
+  "Madness at the Darkmoon Faire",
+  "Fractured in Alterac Valley",
+  "Voyage to the Sunken City",
+];
+
+export default cardSets;
---
+++ src/components/options/classes.ts
@@ -0,0 +1,15 @@
+const classes = [
+    'Death Knight',
+    'Demon Hunter',
+    'Druid',
+    'Hunter',
+    'Mage',
+    'Paladin',
+    'Priest',
+    'Rogue',
+    'Shaman',
+    'Warlock',
+    'Warrior'
+]
+
+export default classes
