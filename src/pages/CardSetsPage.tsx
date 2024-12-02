8323355fe1560c942c9564ce385526b473835ac7
Pdwxn <pdwxn@protonmail.com>
2024-12-02 17:42:59 -0500

    New pages

--- src/pages/CardSetsPage.tsx
+++ src/pages/CardSetsPage.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Navbar from "../components/Layout/navbar/NavBar";
 import SearchBar from "../components/Layout/navbar/SearchBar";
 import Footer from "../components/Layout/footer/Footer";
+import CardSetList from "../components/content/CardSetList";
+import Selector from "../components/Selector";
+import cardSets from "../components/options/cardSet";
 
 interface Props {
   query: string;
@@ -10,6 +13,7 @@ interface Props {
 
 function CardSetsPage({ query, setQuery }: Props) {
   const [loading, setLoading] = useState<boolean>(false);
+  const [cardSetName, setCardSetName] = useState<string>("");
 
   return (
     <>
@@ -19,7 +23,26 @@ function CardSetsPage({ query, setQuery }: Props) {
         </Navbar>
       </nav>
       <div className="ml-14 mr-14">
-        <h1 className="mb-8 text-4xl text-slate-50 font-semibold">Card Sets</h1>
+        <div className="flex flex-wrap object-center gap-15 justify-between items-center">
+          <h1 className="mb-8 text-4xl text-slate-50 font-semibold">
+            Card Sets
+          </h1>
+          <div className="mb-8 flex flex-wrap items-center gap-4">
+            <label className="text-lg text-slate-50">Select a Card Set: </label>
+            <Selector
+            selectedSet={cardSetName}
+            onSetChange={setCardSetName}
+            options={cardSets}
+          />
+          </div>
+        </div>
+        {cardSetName && (
+        <CardSetList
+          loading={loading}
+          setLoading={setLoading}
+          setName={cardSetName}
+        />
+      )}
       </div>
       <Footer />
     </>
--- src/pages/ClassesPage.tsx
+++ src/pages/ClassesPage.tsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import Navbar from "../components/Layout/navbar/NavBar";
 import SearchBar from "../components/Layout/navbar/SearchBar";
 import Footer from "../components/Layout/footer/Footer";
+import CardClassesList from "../components/content/CardClassesList";
+import Selector from "../components/Selector";
+import classes from "../components/options/classes";
+
 
 interface Props {
   query: string;
@@ -10,6 +14,7 @@ interface Props {
 
 function ClassesPage({ query, setQuery }: Props) {
   const [loading, setLoading] = useState<boolean>(false);
+  const [cardClassName, setCardClassName] = useState<string>("");
   
   return (
     <>
@@ -19,8 +24,26 @@ function ClassesPage({ query, setQuery }: Props) {
         </Navbar>
       </nav>
       <div className="ml-14 mr-14">
-        <h1 className="mb-8 text-4xl text-slate-50 font-semibold">Classes</h1>
-        
+      <div className="flex flex-wrap object-center gap-15 justify-between items-center">
+          <h1 className="mb-8 text-4xl text-slate-50 font-semibold">
+            Classes
+          </h1>
+          <div className="mb-8 flex flex-wrap items-center gap-4">
+            <label className="text-lg text-slate-50">Select a Class: </label>
+            <Selector
+            selectedSet={cardClassName}
+            onSetChange={setCardClassName}
+            options={classes}
+          />
+          </div>
+        </div>
+        {cardClassName && (
+        <CardClassesList
+          loading={loading}
+          setLoading={setLoading}
+          setClass={cardClassName}
+        />
+      )}
       </div>
       <Footer />
     </>
